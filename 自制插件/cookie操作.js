2cd3d4177e438d99600bc01b0b248706a360992d
普普 <maoway42@gmail.com>
2017-04-27 23:06:59 +0800

    Update cookie操作.js

--- 自制插件/cookie操作.js
+++ 自制插件/cookie操作.js
@@ -1,9 +1,10 @@
+//参数:键,值,保存天数
 function setCookie(name, value, iDay) {
     var mydate = new Date();
     mydate.setDate(mydate.getDate() + iDay);
     document.cookie = name + '=' + value + ';expires=' + mydate;
 }
-
+//cookie保存形式:name=***; keyword=***
 function getCookie(name) {
     var arr = document.cookie.split('; ');
     for (var i=arr.length-1; i>=0; i--) {
@@ -14,7 +15,7 @@ function getCookie(name) {
     }
     return '未找到该参数';
 }
-
+//日期-1即已过期
 function removeCookie(name) {
     setCookie(name, 1, -1);
 }
