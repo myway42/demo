1657a3c439fd561067c77f0a51c4382cb9589443
myway42 <maoway42@gmail.com>
2017-07-24 21:27:15 +0800

    add doubanbook

---
+++ doubanbook/pages/detail/detail.js
@@ -0,0 +1,32 @@
+var requests = require( '../../requests/request.js' );
+
+Page( {
+  data: {
+    id: null,
+    loadidngHidden: false,
+    bookData: null
+  },
+  onLoad: function( option ) {
+    this.setData({
+      id: option.id
+    });
+  },
+  onReady: function() {
+    var id = this.data.id;
+    var _this = this;
+    requests.requestBookDokDetail(
+      id, 
+      {fields: 'image,summary,publisher,title,rating,pubdate,author,author_intro,catalog'}, 
+      ( data ) => {
+        _this.setData({
+          bookData: data
+        });
+    }, () => {
+      wx.navigateBack();
+    }, () => {
+      _this.setData( {
+        loadidngHidden: true
+      });
+    });
+  }
+});
